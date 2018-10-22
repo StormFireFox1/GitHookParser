880db17e44b1d837131766b9ef4659d1688dddf6
StormFireFox1 <matei@gardus.eu>
2018-10-22 11:19:04 +0300

    Create runnable executable

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/stormfirefox1/GitHookParser/server"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/stormfirefox1/GitHookParser/config"
+	"github.com/stormfirefox1/GitHookParser/log"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Printf("GitHookParser v.%s \n", config.Get("APP_VERSION"))
+	log.Info(logrus.Fields{
+		"bootTime": time.Now(),
+		"version":  config.Get("APP_VERSION"),
+	}, "Booting up...")
+
+	server := server.server{
+		env: config.New("$HOME/.config/git-hook-parser"),
+	}
+
+	server.routes()
+
+	http.ListenAndServe(config.Get("HANDLER_PORT"), server.router)
 }
